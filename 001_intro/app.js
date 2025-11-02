5ba9b7d72ed7dcd91b6f5386defbdef89a76f6cd
olbap <olbaplegal@gmail.com>
2025-11-02 14:52:19 -0300

    implementado endpoint '/'

--- 001_intro/app.js
+++ 001_intro/app.js
@@ -11,3 +11,14 @@ const express = require('express');
 
 //Instância do express
 const app = express();
+
+/*
+>O método GET é usado quando um cliente (como o navegador) quer obter dados de um servidor
+>"/" é o primeiro argumento, ele define o caminho da URL, quando é apenas eles significa que é a raiz do site
+>function(req, res){...} - Esta é a função de callback. É o "o que fazer" quando um evento GET acontece, essa 
+função não é ativada quando o servidor liga, mas sim toda vez que um usuário acessa essa rota específica.
+>res.send() instrui o servidor a enviar uma resposta.
+*/
+app.get("/", function(req, res){
+    res.send("<h1>Hello World!<h1>") //Nesse caso a resposta do server foi um h1 com o conteúdo
+})
