f6a47e0d4261eae612d10b15c6503a71b0e597db
olbap <olbaplegal@gmail.com>
2025-11-02 14:43:06 -0300

    implementado importação de modulos e instanciando objeto

--- 001_intro/app.js
+++ 001_intro/app.js
@@ -5,3 +5,9 @@ npm install express --save
 npm install express ejs
 npm install open
 */
+
+// require("express") é a forma como o Node.js importa módulos, bibliotecas e pacotes
+const express = require('express');
+
+//Instância do express
+const app = express();
