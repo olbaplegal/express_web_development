97d112ac9ab5fcd6b8d21eb30badb58d8a460ad0
olbap <olbaplegal@gmail.com>
2025-11-04 10:40:50 -0300

    baixando dependencias

---
+++ 002_async/app.js
@@ -0,0 +1,8 @@
+/*
+    INSTALAR VIA TERMINAL VS CODE
+    npm init -y: cria um arquivo package.json
+    npm install express --save: instalando o express e --save guarda o express como dependencia em packages.json
+    npm install express ejs: ejs para criar HTML dinâmicos
+    npm install open: pacote open permite abrir aplicações, URL e arquivos
+*/
+
