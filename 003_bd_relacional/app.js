1eb8bdb5ea6cfb6a9e36b1ea4238291d5195a84a
olbap <olbaplegal@gmail.com>
2025-11-04 14:18:47 -0300

    botando os pacotes em variaveis

--- 003_bd_relacional/app.js
+++ 003_bd_relacional/app.js
@@ -5,3 +5,8 @@ npm install express ejs
 npm install mysql2: conexão com o BD
 npm install open
 */
+const express = require('express'); // importando express
+const mysql = require('mysql2'); // importando mysql
+const app = express(); // criando uma instancia do express
+const port = 3000; // porta
+
