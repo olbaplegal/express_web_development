a872c951ffc505c33d6c37ab0428af9d88b42463
olbap <olbaplegal@gmail.com>
2025-11-04 10:54:08 -0300

    setando configurações do app

--- 002_async/app.js
+++ 002_async/app.js
@@ -6,3 +6,16 @@
     npm install open: pacote open permite abrir aplicações, URL e arquivos
 */
 
+// path -> ferramenta para trabalhar com caminhos de arquivos e diretórios
+const express = require("express"); // Importando modulos
+const path = require("path"); // caminho, é como se fosse um "url_for" do python
+const app = express(); // Instancia do express
+const port = 8081 // Porta
+
+app.set('view engine', 'ejs'); // Configurando o EJS como motor de visualização
+
+//__dirname: caminho absoluto do diretorio(atual)
+//path.join(): usa caminhos de forma segura. usa / ou \ dependendo do SO
+app.set('views', path.join(__dirname, 'views')); // configurando views
+
+app.use(express.static(path.join(__dirname, 'public'))); // configurando arquivo statico
