6f63c829035b526580744f211f04ffee7bf0def1
olbap <olbaplegal@gmail.com>
2025-11-06 14:27:05 -0300

    baixando dependencias

---
+++ 004_puppeteer/app.js
@@ -0,0 +1,10 @@
+/*
+INSTALAR VIA TERMINAL VS CODE
+npm init -y
+npm install express --save
+npm install express ejs
+npm install mysql2
+npm install puppeteer: pacote para fazer o web scrap
+npm install axios: vai fazer uma requisição pra uma API de cotação para transformar o preço em BRL
+npm install open
+*/
