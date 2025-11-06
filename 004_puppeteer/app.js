07401fd0e027a36ffd31bc34abb2c3b792be87f3
olbap <olbaplegal@gmail.com>
2025-11-06 14:34:13 -0300

    criada string de conexão com o BD

--- 004_puppeteer/app.js
+++ 004_puppeteer/app.js
@@ -8,3 +8,17 @@ npm install puppeteer: pacote para fazer o web scrap
 npm install axios: vai fazer uma requisição pra uma API de cotação para transformar o preço em BRL
 npm install open
 */
+
+const puppeteer = require("puppeteer");
+const mysql = require("mysql2/promise");
+const axios = require("axios")
+
+async function scrap(){
+    const dbConfig = {
+        host: 'localhost',
+        user: 'root',
+        password: 'password',
+        database: 'biblioteca'
+    };
+    
+}
